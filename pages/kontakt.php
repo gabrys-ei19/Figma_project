eab793315c5657e90129ff8ca855d1cb2e71d940
gabrys-ei19 <102586104+gabrys-ei19@users.noreply.github.com>
2023-04-17 10:48:48 +0200

    adding kontakt.php

---
+++ pages/kontakt.php
@@ -0,0 +1,60 @@
+<div id=container>
+<aside>
+    <?php
+        $add = 'media/img/add/add'.rand(1,3).'.jpg';
+        echo '<img class="add" src="'.$add.'" alt="Logo ZTM" />';
+    ?>
+</aside>
+<main>
+    Witaj na stronie kontaktowej ZTM - Obręb Zadupie! Jesteśmy zawsze gotowi, aby pomóc Ci w rozwiązaniu problemów związanych z transportem miejskim. Poniżej znajdziesz informacje na temat sposobów kontaktu z nami.
+    <br><br>
+    <b>Adres siedziby:</b>
+    ZTM - Obręb Zadupie
+    ul. Transportowa 123
+    12-345 Zadupie
+    <br>
+    <b>Adres e-mail:</b>
+    Kontakt@ztm-zadupie.pl
+    <br>   
+    <b>Numer telefonu:</b>
+    +48 123 456 789
+    <br>
+    <b>Godziny otwarcia biura obsługi klienta:</b>
+    <table>
+        <tr>
+            <td>Poniedziałek: </td>
+            <td>8:00 - 18:00</td>
+        </tr>
+        <tr>
+            <td>Wtorek: </td>
+            <td>8:00 - 18:00</td>
+        </tr>
+        <tr>
+            <td>Środa: </td>
+            <td>8:00 - 18:00</td>
+        </tr>
+        <tr>
+            <td>Czwartek: </td>
+            <td>8:00 - 18:00</td>
+        </tr>
+        <tr>
+            <td>Piątek: </td>
+            <td>8:00 - 18:00</td>
+        </tr>
+        <tr>
+            <td>Sobota: </td>
+            <td>9:00 - 14:00</td>
+        </tr>
+        <tr>
+            <td>Niedziela: </td>
+            <td>nieczynne</td>
+        </tr>
+    </table>
+    <br>
+    Jeśli masz jakieś pytania lub wątpliwości, prosimy o kontakt z naszym biurem obsługi klienta w godzinach pracy. Jesteśmy dostępni przez telefon oraz e-mail. Chętnie udzielimy Ci wszelkich informacji i pomocy w kwestiach związanych z transportem miejskim w Obrębie Zadupie.
+    <br><br>
+    Jeśli chcesz złożyć skargę lub sugestię dotyczącą naszych usług, możesz zrobić to również poprzez e-mail lub telefonicznie. Staramy się odpowiedzieć na wszystkie wiadomości jak najszybciej i jak najskuteczniej rozwiązać wszelkie problemy.
+    <br><br>
+    Dziękujemy za korzystanie z naszych usług i czekamy na Twoje pytania i sugestie!    
+</main>
+</div>
